9be1baaf779382478ea5405aec3a6ea17d3a495e
amogh7joshi <joshi.amoghn@gmail.com>
2021-02-23 09:46:51 -0500

    Added method to plot dataset class distribution

---
+++ preprocessing/distribution.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding = utf-8 -*-
+import os
+import json
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_json_dicts():
+   """Loads the JSON dictionaries containing the train/val/test paths and class labels."""
+   # Load the dictionaries from their paths.
+   with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/Dataset/train.json')) as f:
+      train_json_dict = json.load(f)
+   with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/Dataset/train.json')) as f:
+      val_json_dict = json.load(f)
+
+   # Return the dictionaries.
+   return train_json_dict, val_json_dict
+
+def get_unique_classes(train, val):
+   """Returns the unique classes and counts from the train/val image IDs."""
+   # Create a list of classes.
+   classes = []
+
+   # Iterate over train/val.
+   for dict in [train, val]:
+      # Iterate over the items within the dictionary.
+      for item in dict:
+         # Add the classes to the list.
+         classes.extend(item['classes'])
+
+   return np.unique(classes, return_counts = True)
+
+def plot_distribution(unique_classes, class_counts, background = 'light'):
+   """Plots the frequency distribution of each class in the dataset."""
+   # Prettify the class names.
+   unique_classes = [item.replace("_", " ").title() for item in unique_classes]
+   class_counts = [item for item in class_counts]
+
+   # Change the order from greatest to least.
+   class_counts, unique_classes = zip(*reversed(sorted(zip(class_counts, unique_classes))))
+
+   # Construct the figure.
+   fig, ax = plt.subplots(figsize = (8, 8))
+   plt.title(r'$\bf{Dataset\;Class\;Imbalance}$')
+   if background == "dark":
+      fig.patch.set_facecolor('#2e3037ff')
+   elif background == "light":
+      fig.patch.set_facecolor('#efefefff')
+
+   # Plot the distribution.
+   ax.bar(np.arange(len(class_counts)), class_counts,
+          color = ['cornflowerblue', 'orangered', 'deepskyblue', 'aquamarine', 'darkorange', 'springgreen'])
+
+   # Configure the x- and y-axis.
+   plt.xticks(np.arange(len(class_counts)), unique_classes)
+
+   # Display the plot.
+   plt.show()
+
+if __name__ == '__main__':
+   # Load the JSON dictionaries.
+   train_dict, val_dict = get_json_dicts()
+
+   # Get the unique classes and counts.
+   classes, counts = get_unique_classes(train_dict, val_dict)
+
+   # Plot the unique classes frequency distribution.
+   plot_distribution(classes, counts)
