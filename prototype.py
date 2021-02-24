bfd44393179b7684310f8407df06328408054ece
amogh7joshi <joshi.amoghn@gmail.com>
2021-02-24 16:40:23 -0500

    Add method to display segmented maps directly from prototype model

--- prototype.py
+++ prototype.py
@@ -11,9 +11,9 @@ from tensorflow.keras.models import load_model
 
 from model.loss import dice_loss_2d, surface_channel_loss_2d
 from preprocessing.dataset import AgricultureVisionDataset
-from testing.image import get_testing_image
+from testing.image import get_testing_image, create_displayable_test_output
 from testing.process import preprocess_image, postprocess_output
-from testing.segment import draw_segmentation_map, display_segmented_pair
+from testing.segment import draw_segmentation_map, display_segmented_diagram
 
 class FarmlandAnomalyModel(object):
    """The complete container class for the farmland anomaly segmentation model."""
@@ -21,6 +21,23 @@ class FarmlandAnomalyModel(object):
       # Set up the actual model which is being stored in the container.
       self.model = None
       self._initialize_model(model, **kwargs)
+
+   @staticmethod
+   def multi_initialize(modes):
+      """Initializes multiple models at the same time for convenience."""
+      _returnable_models = []
+
+      # Create a list of models for each of the provided modes.
+      for mode in modes:
+         try:
+            _mode_model = FarmlandAnomalyModel(model = mode)
+         except Exception as e:
+            raise e
+         else:
+            _returnable_models.append(_mode_model)
+
+      # Return the models.
+      return _returnable_models
 
    def __str__(self):
       # Print out the summary of the model architecture.
@@ -49,6 +66,9 @@ class FarmlandAnomalyModel(object):
             else:
                # Otherwise, just load the model.
                self.model = load_model(model)
+         else:
+            raise FileNotFoundError(f"Received a model path ending with .hdf5 ({model}), but "
+                                    f"the path to the model does not exist.")
       else:
          # Otherwise, we've gotten a shortcut model name in which case load that specific one.
          if model.lower() in ['dice20', '20', 'first', 'stage1']:
@@ -96,13 +116,54 @@ class FarmlandAnomalyModel(object):
       # Get a valid testing image.
       if isinstance(test_image, (list, tuple, set)):
          # If we're using an item from the dataset, then get the specific dataset item.
-         testing_image = get_testing_image(mode = test_image[0], value = test_image[1], with_truth = False)
+         testing_image = get_testing_image(
+            mode = test_image[0], value = test_image[1], with_truth = False)
       else:
          # Otherwise, preprocess the input image.
          testing_image = preprocess_image(test_image)
 
       # Now, predict the output from the model and return postprocessed predictions.
       return postprocess_output(self.model.predict(testing_image))
+
+   def show_segmented_predictions(self, test_image, with_truth = True):
+      """Displays the segmented model predictions."""
+      if isinstance(test_image, (list, tuple, set)):
+         # If we're using an item from the dataset, then we need to get the specific dataset item.
+         if with_truth:
+            # Get the ground truth too if requested to.
+            testing_image, testing_truth = get_testing_image(
+               mode = test_image[0], value = test_image[1], with_truth = True)
+         else:
+            # Get the ground truth too if requested to.
+            testing_image = get_testing_image(
+               mode = test_image[0], value = test_image[1], with_truth = False)
+      else:
+         # Otherwise, just process the test image.
+         testing_image = test_image
+
+      # Get the model predictions.
+      predicted = self.model.predict(testing_image)
+      predicted = postprocess_output(predicted)
+
+      # Convert the test image/label into usable images.
+      displayable_test_image = create_displayable_test_output(test_image)
+      if with_truth:
+         testing_truth = postprocess_output(testing_truth)
+
+      # Draw the contours onto the main image.
+      annotated_test_prediction = draw_segmentation_map(displayable_test_image.copy(), predicted)
+      if with_truth:
+         annotated_test_truth = draw_segmentation_map(displayable_test_image.copy(), testing_truth)
+
+      # Display the diagram.
+      if with_truth:
+         display_segmented_diagram(displayable_test_image, annotated_test_prediction, annotated_test_truth)
+      else:
+         display_segmented_diagram(displayable_test_image, annotated_test_prediction)
+
+
+
+
 
 
 
