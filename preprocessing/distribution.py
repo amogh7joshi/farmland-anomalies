6706e84eacc1051d5a557f1407b44a34fa465c2f
amogh7joshi <joshi.amoghn@gmail.com>
2021-03-31 14:25:15 -0400

    Added pie chart diagram for dataset class distribution

--- preprocessing/distribution.py
+++ preprocessing/distribution.py
@@ -59,6 +59,31 @@ def plot_distribution(unique_classes, class_counts, background = 'light'):
    # Display the plot.
    plt.show()
 
+def create_pie_distribution(unique_classes, class_counts, background = 'light'):
+   """Constructs a pie chart for the dataset class distribution."""
+   # Prettify the class names.
+   unique_classes = [item.replace("_", " ").title() for item in unique_classes]
+   class_counts = [item for item in class_counts]
+
+   # Construct the figure.
+   fig, ax = plt.subplots(figsize = (10, 10))
+   plt.title(r'$\bf{Dataset\;Class\;Imbalance}$', fontsize = 20)
+   if background == "dark":
+      fig.patch.set_facecolor('#2e3037ff')
+   elif background == "light":
+      fig.patch.set_facecolor('#efefefff')
+
+   # Plot the distribution.
+   ax.pie(class_counts, labels = unique_classes, shadow = True,
+          autopct = '%1.1f%%', startangle = 90, textprops = {'fontsize': 13})
+   ax.axis('equal')
+
+   # Configure the rest of the plot.
+   ax.legend()
+
+   # Display the plot.
+   plt.show()
+
 if __name__ == '__main__':
    # Load the JSON dictionaries.
    train_dict, val_dict = get_json_dicts()
@@ -67,4 +92,4 @@ if __name__ == '__main__':
    classes, counts = get_unique_classes(train_dict, val_dict)
 
    # Plot the unique classes frequency distribution.
-   plot_distribution(classes, counts)
+   create_pie_distribution(classes, counts)
